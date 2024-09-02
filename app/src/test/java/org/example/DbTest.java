0a507936a82b6e22c8c8368af2fce65e56fbb384
Trevor Durán <trevor.m.duran@gmail.com>
2024-09-02 15:24:50 -0600

    feat: add multiple entries for simple test

--- app/src/test/java/org/example/DbTest.java
+++ app/src/test/java/org/example/DbTest.java
@@ -3,23 +3,46 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class DbTest {
+    Db db;
+
+    @BeforeEach
+    void setUp() {
+        db = new Db();
+    }
+
     @Test
     void dbSimpleGetAndDelete() {
         // Arrange
-        String namespace = "Users";
-        String key = "name";
-        String val = "Trevor";
-        Db db = new Db();
+        String namespaceA = "Users";
+        String keyA = "name";
+        String valA = "Trevor";
+        String namespaceB = "Cars";
+        String keyB = "name";
+        String valB = "Trevor";
         // Assert the database does not contain data to begin with
-        assertNull(db.get(namespace, key));
+        assertNull(db.get(namespaceA, keyA));
         // Act by setting the expected data into the database
-        db.set(namespace, key, val);
+        db.set(namespaceA, keyA, valA);
+        db.set(namespaceB, keyB, valB);
         // Assert the expected data in database is correct
-        assertEquals(val, db.get(namespace, key));
+        assertEquals(valA, db.get(namespaceA, keyA));
         // Act by removing the value in the database
-        db.delete(namespace, key);
+        db.delete(namespaceA, keyA);
         // Assert the data is removed (null) as expected
-        assertNull(db.get(namespace, key));
+        assertNull(db.get(namespaceA, keyA));
+
+        assertEquals(valB, db.get(namespaceB, keyB));
     }
+
+    // void dbGetAll() {
+    // String namespaceInsect = "Insects";
+    // String keyName = "name";
+    // String valBeetle = "Beetle";
+    // assertNull(db.get(namespaceInsect, keyName));
+    // db.
+
+    // }
 }
