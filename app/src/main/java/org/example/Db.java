9e2ca9bd1c42931832b9248638152102991a990b
Trevor Durán <trevor.m.duran@gmail.com>
2024-09-02 15:25:53 -0600

    feat: update set, get, and delete using new classes

--- app/src/main/java/org/example/Db.java
+++ app/src/main/java/org/example/Db.java
@@ -1,33 +1,81 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Db implements iDb {
 
-    private HashMap<String, HashMap<String, String>> data;
+    private HashMap<String, DbTable> data;
 
     public Db() {
-        data = new HashMap<String, HashMap<String, String>>();
+        data = new HashMap<String, DbTable>();
     }
 
     public void set(String namespace, String key, String val) {
+        DbTable table;
+        DbEntry entry;
+
         if (!data.containsKey(namespace)) {
-            data.put(namespace, new HashMap<String, String>());
+            table = new DbTable(namespace);
+            data.put(namespace, table);
+        } else {
+            table = data.get(namespace);
         }
-        data.get(namespace).put(key, val);
+
+        if (!table.hasEntry(key)) {
+            entry = new DbEntry(key, val);
+            table.addEntry(entry);
+        } else {
+            entry = table.getEntry(key);
+        }
+
+        entry.setValue(val);
     }
 
     public String get(String namespace, String key) {
-        if (!data.containsKey(namespace) || !data.get(namespace).containsKey(key)) {
+        if (!data.containsKey(namespace) || !data.get(namespace).hasEntry(key)) {
             return null;
         }
-        return data.get(namespace).get(key);
+        return data.get(namespace).getEntry(key).getValue();
     }
 
     public void delete(String namespace, String key) {
-        if (!data.containsKey(namespace) || !data.get(namespace).containsKey(key)) {
+        DbTable table;
+        DbEntry entry;
+        if (!data.containsKey(namespace)) {
             return;
         }
-        data.get(namespace).remove(key);
+        table = data.get(namespace);
+
+        if (!table.hasEntry(key)) {
+            return;
+        }
+        entry = table.getEntry(key);
+
+        table.removeEntry(entry);
+    }
+
+    public String getAll(String namespace) {
+        if (!data.containsKey(namespace)) {
+            return "";
+        }
+        return "";
+    }
+
+    public String getAll(String namespace, String prefix) {
+        if (!data.containsKey(namespace)) {
+            return "";
+        }
+        return "";
+    }
+
+    @Override
+    public String toString() {
+        String dataStr = "";
+        for (Map.Entry<String, DbTable> table : data.entrySet()) {
+            dataStr += table.toString();
+        }
+
+        return String.format("The current db table: %s", dataStr);
     }
 }
